044f7e4503174fb23c342a03147f3931a6dd6035
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-04-27 17:32:59 +0300

    Invoke Used(for finish and bump head)

--- SnowBoarderr/Assets/Scripts/CrushDetector.cs
+++ SnowBoarderr/Assets/Scripts/CrushDetector.cs
@@ -5,10 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class CrushDetector : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 0.5f;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Surface"){
-            Debug.Log("Oh no i will choke in the snow.");
+        if (other.tag == "Surface")
+        {
+            Invoke("ReloadScene", loadDelay);
         }
+    }
+
+    void ReloadScene(){
+        SceneManager.LoadScene(0);
     }
 }
--- SnowBoarderr/Assets/Scripts/FinishLine.cs
+++ SnowBoarderr/Assets/Scripts/FinishLine.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float finishDelay = 2f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            Debug.Log(" You finished");
+            Invoke("ReloadScene", finishDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 
 }
