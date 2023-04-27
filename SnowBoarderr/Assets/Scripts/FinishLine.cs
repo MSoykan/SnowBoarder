5973c04ff5ac64ebd38ddd38c55c7564c97349a5
MSoykan <mehmetsoykanmutlu@gmail.com>
2023-04-27 20:45:24 +0300

    added & triggered Particle Effects

--- SnowBoarderr/Assets/Scripts/FinishLine.cs
+++ SnowBoarderr/Assets/Scripts/FinishLine.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float finishDelay = 2f;
+    [SerializeField] ParticleSystem finishEffect;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+            finishEffect.Play();
             Invoke("ReloadScene", finishDelay);
         }
     }
